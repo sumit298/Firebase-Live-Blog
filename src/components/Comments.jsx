5248b972a201e37c8f42ddda111df4148e4f70b1
Sumit <sumitsinhassss@outlook.com>
2020-10-19 18:34:20 +0530

    Console log fix

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AddComment from './AddComment';
 import Comment from './Comment';
 function Comments({ onCreate, comments }) {
-	console.log(comments);
+	// console.log(comments);
 	return (
 		<section className="Comments">
 			<AddComment onCreate={onCreate} />
