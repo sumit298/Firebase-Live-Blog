abcc3c79448b80acfa8419846f36122fa4495a0d
sumit298 <sumitsinhassss@outlook.com>
2022-01-15 11:18:18 +0530

    dependency version changes

--- src/Context/UserProvider.jsx
+++ src/Context/UserProvider.jsx
@@ -1,43 +1,42 @@
-import React, { createContext, useState, useEffect } from 'react';
-import { auth, createUserProfileDocument } from '../firebase';
+import React, { createContext, useState, useEffect } from "react";
+import { auth, createUserProfileDocument } from "../firebase";
 
 export const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-	const [state, setState] = useState({
-		user: null,
-		// userLoaded: true,
-	});
+  const [state, setState] = useState({
+    user: null,
+    // userLoaded: true,
+  });
 
-	useEffect(() => {
-		let unsubscribeFromAuth = null;
+  useEffect(() => {
+    let unsubscribeFromAuth = null;
 
-		const getAuth = async () => {
-			unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-				if(userAuth){
-				const userRef = await createUserProfileDocument(userAuth);
-				userRef.onSnapshot(snapshot => {
-					setState({user: {uid: snapshot.id, ...snapshot.data()}})
-				})
-				// console.log(user);
-				setState({user: userAuth });
-				}
-			});
-		};
+    const getAuth = async () => {
+      unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+        if (userAuth) {
+          const userRef = await createUserProfileDocument(userAuth);
+          userRef.onSnapshot((snapshot) => {
+            setState({ user: { uid: snapshot.id, ...snapshot.data() } });
+          });
+          // console.log(user);
+          setState({ user: userAuth });
+        }
+      });
+    };
 
-		getAuth();
-		return () => {
-			unsubscribeFromAuth();
-		};
-	}, []);
+    getAuth();
+    return () => {
+      unsubscribeFromAuth();
+    };
+  }, []);
 
-	const { user } = state;
+  const { user } = state;
 
-	return (
-		<UserContext.Provider value={{ user }}>
-			{children}
-		</UserContext.Provider>
-	);
+
+  return (
+    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
+  );
 };
 
 export default UserProvider;
